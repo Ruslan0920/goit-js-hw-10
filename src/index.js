b26ce6a19208c1164159a66d72cf298173f547ec
Ruslan0920 <rus_boy2002@ukr.net>
2023-09-22 15:55:46 +0300

    summary upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('HELLO WORLD');
+import axios from "axios";
+import { TheCatAPI } from "@thatapicompany/thecatapi";
+
+const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq");
+console.log(theCatAPI);
+
+fetch('https://api.thecatapi.com/v1/images/search?api_key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq')
