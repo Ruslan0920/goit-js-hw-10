7b84dde8e7891f252a078866c9ba1142eea531de
Ruslan0920 <rus_boy2002@ukr.net>
2023-09-25 15:10:41 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4D
 console.log(theCatAPI);
 
 fetch('https://api.thecatapi.com/v1/images/search?api_key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq')
+    .then(resp => {
+        return resp.json();
+    })
+    .then(cats => {
+    console.log(cats);
+    })
+    .catch(error => {
+    console.log(error);
+})
