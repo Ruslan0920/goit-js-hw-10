8c70c2bda18894f52416581c39be318368ad4a88
Ruslan0920 <rus_boy2002@ukr.net>
2023-09-25 16:31:55 +0300

    up1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { TheCatAPI } from "@thatapicompany/thecatapi";
 
+const cardContainer = document.querySelector('.breed-select');
+console.log(cardContainer);
+
 const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq");
 console.log(theCatAPI);
 
@@ -13,4 +16,15 @@ fetch('https://api.thecatapi.com/v1/images/search?api_key=live_58gwQPyj3Dq4FvqlC
     })
     .catch(error => {
     console.log(error);
-})
+    })
+
+    theCatAPI.images
+  .searchImages({
+    limit: 6,
+  })
+  .then((images) => {
+    console.log(images);
+  })
+  .catch((error) => {
+    // handle error
+  });
