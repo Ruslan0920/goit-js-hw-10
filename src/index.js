bc31a90ddf95f85fd6a4d66c6d3607ba425e4505
Ruslan0920 <rus_boy2002@ukr.net>
2023-09-27 17:02:37 +0300

    new upd

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import axios from "axios";
-// console.log(axios);
+console.log(axios);
 
-import '../src/cat-api.js'
+// import '../src/cat-api.js'
 import { TheCatAPI } from "@thatapicompany/thecatapi";
 
 const cardContainer = document.querySelector('.breed-select');
 console.log(cardContainer);
+// cardContainer.addEventListener('submit', fetchBreeds)
 
 const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq");
 console.log(theCatAPI);
 
-fetch('https://api.thecatapi.com/v1/images/search?api_key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq')
-    // .then(resp => {
-    //     return resp.json();
-    // })
-    // .then(cats => {
-    // console.log(cats);
-    // })
-    // .catch(error => {
-    // console.log(error);
-    // })
+fetchBreeds()
 
+function fetchBreeds() {
+    fetch('https://api.thecatapi.com/v1/breeds?api_key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq')
+    .then(resp => {
+        return resp.json();
+    })
+    .then(cats => {
+        console.log(cats);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
 //     theCatAPI.images
 //   .searchImages({
 //     limit: 6,
@@ -31,3 +35,24 @@ fetch('https://api.thecatapi.com/v1/images/search?api_key=live_58gwQPyj3Dq4FvqlC
 //   .catch((error) => {
 //     // handle error
 //   });
+
+// const url = 'https://api.thecatapi.com/v1';
+// axios.defaults.headers.common["x-api-key"] = "live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq";
+// const key = 'live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq' ;
+// // console.log(key);
+
+
+
+// function fetchBreeds() {
+//     return fetch(`${url}/breeds?api_key=${key}`)
+//         .then(resp => {
+//         return resp.json();
+//     })
+//     .then(cats => {
+//     console.log(cats);
+//     })
+//     .catch(error => {
+//     console.log(error);
+//     })
+//     // return response.json();
+// }
