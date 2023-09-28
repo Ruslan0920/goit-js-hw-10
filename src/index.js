089a078744d933a9e00ab09f37ae564c35699481
Ruslan0920 <rus_boy2002@ukr.net>
2023-09-28 17:04:45 +0300

    upd01

--- src/index.js
+++ src/index.js
@@ -13,18 +13,38 @@ console.log(theCatAPI);
 
 fetchBreeds()
 
+let massiveCats = []
+console.log(massiveCats);
+
 function fetchBreeds() {
-    fetch('https://api.thecatapi.com/v1/breeds?api_key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq')
-    .then(resp => {
+   return fetch('https://api.thecatapi.com/v1/breeds?api_key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq')
+       .then(resp => {
         return resp.json();
     })
     .then(cats => {
         console.log(cats);
+        cats.forEach(cat => {
+            // console.log(cat);
+            // console.log(`значення: ${cat.id}, Ім'я: ${cat.name}`);
+//   console.log(`значення: ${cat.id}, Ім'я: ${cat.name}, Oпис: ${cat.description}`);
+ massiveCats.push({text: cat.name, value: cat.id})           
+});
+        
     })
     .catch(error => {
         console.log(error);
     })
+    
 }
+
+function fetchCatByBreed(breedId) {
+    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+        .then(response => {
+return response.json();
+    });  
+};
+
+
 //     theCatAPI.images
 //   .searchImages({
 //     limit: 6,
@@ -56,3 +76,41 @@ function fetchBreeds() {
 //     })
 //     // return response.json();
 // }
+
+// const cats = [
+//     {
+//         id: "asd",
+//         name: "Cat1"
+//     },
+//        {
+//         id: "qwe",
+//         name: "Cat2"
+//     },
+//         {
+//         id: "zxc",
+//         name: "Cat3"
+//     },
+//         {
+//         id: "rty",
+//         name: "Cat4"
+//     },
+//         {
+//         id: "fgh",
+//         name: "Cat5"
+//     },
+//         {
+//         id: "vbn",
+//         name: "Cat6"
+//     },    
+    
+// ];
+
+// // Класичний for
+// // for (let i = 0; i < numbers.length; i += 1) {
+// //   console.log(`Індекс ${i}, значення ${numbers[i]}`);
+// // }
+
+// // Метод перебирання forEach
+// cats.forEach(function (cat) {
+//   console.log(`значення ${cat.id}, Ім'я ${cat.name}`);
+// });
