a27010caa07abffef48fa5dda6d9a3989bbbd84f
Ruslan0920 <rus_boy2002@ukr.net>
2023-09-29 11:01:13 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,48 +1,84 @@
 import axios from "axios";
-console.log(axios);
+// console.log(axios);
+axios.defaults.headers.common["x-api-key"] = "live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq";
 
 // import '../src/cat-api.js'
 import { TheCatAPI } from "@thatapicompany/thecatapi";
 
 const cardContainer = document.querySelector('.breed-select');
 console.log(cardContainer);
-// cardContainer.addEventListener('submit', fetchBreeds)
+cardContainer.addEventListener('submit', selectCats)
 
-const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq");
-console.log(theCatAPI);
 
-fetchBreeds()
+// const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq");
+// console.log(theCatAPI);
 
 let massiveCats = []
 console.log(massiveCats);
 
+// fetchBreeds()
+
+
+
+// function fetchBreeds() {
+//    return fetch('https://api.thecatapi.com/v1/breeds')
+//        .then(resp => {
+//         return resp.json();
+//     })
+//     .then(cats => {
+//         console.log(cats);
+//         cats.forEach(cat => {
+//             // console.log(cat.id);
+//             // console.log(`значення: ${cat.id}, Ім'я: ${cat.name}`);
+// //   console.log(`значення: ${cat.id}, Ім'я: ${cat.name}, Oпис: ${cat.description}`);
+//             massiveCats.push({ text: cat.name, value: cat.id })
+//             // cardContainer.value = cat.id;
+//             // console.log(cardContainer.value);
+            
+// });
+        
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+    
+// }
+
 function fetchBreeds() {
-   return fetch('https://api.thecatapi.com/v1/breeds?api_key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq')
-       .then(resp => {
-        return resp.json();
-    })
+    return fetch(`https://api.thecatapi.com/v1/breeds`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });       
+};
+
+fetchBreeds()
     .then(cats => {
         console.log(cats);
         cats.forEach(cat => {
-            // console.log(cat);
+            // console.log(cat.id);
             // console.log(`значення: ${cat.id}, Ім'я: ${cat.name}`);
-//   console.log(`значення: ${cat.id}, Ім'я: ${cat.name}, Oпис: ${cat.description}`);
- massiveCats.push({text: cat.name, value: cat.id})           
-});
-        
-    })
-    .catch(error => {
-        console.log(error);
-    })
-    
+            //   console.log(`значення: ${cat.id}, Ім'я: ${cat.name}, Oпис: ${cat.description}`);
+            massiveCats.push({ text: cat.name, value: cat.id })
+            // cardContainer.value = cat.id;
+            // console.log(cardContainer.value);
+            cardContainer = massiveCats;
+        })
+    });
+
+function selectCats(event) {
+    event.preventDefault();
+    console.log(event);
 }
 
-function fetchCatByBreed(breedId) {
-    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
-        .then(response => {
-return response.json();
-    });  
-};
+// function fetchCatByBreed(breedId) {
+//     return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+//         .then(response => {
+// return response.json();
+//     });  
+// };
 
 
 //     theCatAPI.images
