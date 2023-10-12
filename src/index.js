0022fef29a98b42ce1ceb35987be266877a8f3d0
Ruslan0920 <rus_boy2002@ukr.net>
2023-10-12 15:46:14 +0300

    step1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import SlimSelect from 'slim-select'
+import '../node_modules/slim-select/dist/slimselect.css'
+
 const cardContainer = document.querySelector('.breed-select');
 console.log(cardContainer);
 const loaderWait = document.querySelector('.loader');
@@ -8,167 +10,9 @@ console.log(errorMessage);
 const descriptionCatInfo = document.querySelector('cat-info');
 console.log(descriptionCatInfo);
  
-// cardContainer.addEventListener("submit", selectCat);
-
-// const books = [
-//     {
-//         title: "The Last Kingdom",
-//         author: "Bernard Cornwell",
-//         rating: 8.38,
-//     },
-//     {
-//         title: "На березі спокійних вод",
-//         author: "Роберт Шеклі",
-//         rating: 8.51,
-//     },
-//     {
-//         title: "Сон смішної людини",
-//         author: "Федір Достоєвський",
-//         rating: 7.75,
-//     },
-//     {
-//         title: "Великі сподівання",
-//         author: "Чарльз Діккенс",
-//         rating: 8.75,
-//     },
-// ];
-// console.log(books);
-
-// let massive = [];
-// console.log(massive);
-// console.log(massive[0]);
-// // const markup = massive.map(element => element[0]);
-// // console.log(markup);
-// // cardContainer.innerHTML = markup
+cardContainer.addEventListener("change", selectCat);
 
 
-// // const createObj = books.map(book => `<option value="${book.title}">${book.author}</option>`
-// // ).join(" ");
-// // console.log(createObj);
-// // cardContainer.innerHTML = createObj;
-
-// const addObj = books.forEach(item => {
-//     `<option value="${item.title}">${item.author}</option>`;
-//     console.log(item);
-//     massive.push({ name: item.author, value: item.title });
-//     cardContainer.innerHTML = massive;
-// })
-
-// console.log(addObj);
-
-// const createObj = books.map(book => {`<option value="${book.title}">${book.author}</option>`
-//     // console.log(book);
-//     // console.log(book.author);
-//     // massive.push({ name: book.author, value: book.title });
-//     //     new SlimSelect({
-//     //   select: book.author,
-//     //   data: massive,
-//     // });
-// // return massive
-// });
-// cardContainer.innerHTML = createObj;
-// console.log(createObj);
-
-
-
-// const createObj = books.map(book => {
-// console.log(book);
-// }
-    
-// massive.push({ text: book.name, value: book.id });
-    
-    // new SlimSelect({
-    //   select: cardContainer,
-    //   data: massive,
-    // });
-
-
-    // console.log(book);
-    // massive.push(book.author);
-    // cardContainer.innerHTML = `<option value="">${massive}</option>`;
-    // console.log(cardContainer.textContent);
-    // return massive;
-
-    
-    // return massive;
-    // const allAuthors = book.author;
-    // console.log(allAuthors);
-    // const option = document.createElement('option');
-    // console.log(option);
-    // option.value = book.author;
-    // option.innerHTML = book.title;
-    // console.log(option.innerHTML);
-    // cardContainer.textContent = massive;
-    // console.log(cardContainer.textContent);
-    // cardContainer.innerHTML = `<option value="${idx}">${book.author}</option>`;
-    // new SlimSelect({
-    //   select: cardContainer,
-    //   data: massive,
-    // });
-    // return massive
-    // cardContainer.value = allAuthors;
-    // cardContainer.innerHTML = `<option value="">${massive}</option>`;
-
-
-//     cardContainer.innerHTML = `<option value="">${createObj}</option>`;
-// console.log(createObj);
-
-
-
-// function selectCat(event) {
-//     console.log(event);
-// }
-
-
-
-
-// import axios from "axios";
-// // console.log(axios);
-// axios.defaults.headers.common["x-api-key"] = "live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq";
-
-// import '../src/cat-api.js'
-// import { TheCatAPI } from "@thatapicompany/thecatapi";
-
-
-// const cardContainer = document.querySelector('.breed-select');
-// console.log(cardContainer);
-// const divCatInfo = document.querySelector('.cat-info');
-// console.log(divCatInfo);
-// cardContainer.addEventListener('submit', selectCats)
-
-
-// // const theCatAPI = new TheCatAPI("live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq");
-// // console.log(theCatAPI);
-
-
-
-// // fetchBreeds()
-
-
-
-// // function fetchBreeds() {
-// //    return fetch('https://api.thecatapi.com/v1/breeds')
-// //        .then(resp => {
-// //         return resp.json();
-// //     })
-// //     .then(cats => {
-// //         console.log(cats);
-// //         cats.forEach(cat => {
-// //             // console.log(cat.id);
-// //             // console.log(`значення: ${cat.id}, Ім'я: ${cat.name}`);
-// // //   console.log(`значення: ${cat.id}, Ім'я: ${cat.name}, Oпис: ${cat.description}`);
-// //             massiveCats.push({ text: cat.name, value: cat.id })
-// //             // cardContainer.value = cat.id;
-// //             // console.log(cardContainer.value);
-            
-// // });
-        
-// //     })
-// //     .catch(error => {
-// //         console.log(error);
-// //     })
-    
-// // }
 
 let arrCats = []
 console.log(arrCats);
@@ -177,33 +21,31 @@ function fetchBreeds() {
     return fetch(`https://api.thecatapi.com/v1/breeds?key=live_58gwQPyj3Dq4FvqlCn68AVFWNKhsfMh6HOo9fnFo4DK8Vdp1k0H2kuN0I3s8SFgq`)
         .then(response => {
             console.log(response);
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
             return response.json();
         });
 };
 
 fetchBreeds()
-    .then(cat => {
+    .then(cat => {        
         console.log(cat);
         cat.map(cat => {
-            arrCats.push({ text: cat.name, value: cat.id });
-        })
-        // cardContainer.value = arrCats;
-        // cardContainer.innerHTML = cat;
-        new SlimSelect({
-      select: cardContainer,
-      data: arrCats,
-    });
-    });
-    
+            arrCats.push({ text: cat.name});
+        });
 
-// function selectCats(event) {
-//     event.preventDefault();
-//     console.log(event);
-//     divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
-// }
+        new SlimSelect({    
+            select: cardContainer,
+            data: arrCats,
+        });
+    });
+
+
+
+function selectCat(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.currentTarget);
+    // divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+}
 
 // // function fetchCatByBreed(breedId) {
 // //     return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
@@ -245,60 +87,5 @@ fetchBreeds()
 // //     // return response.json();
 // // }
 
-// // const cats = [
-// //     {
-// //         id: "asd",
-// //         name: "Cat1"
-// //     },
-// //        {
-// //         id: "qwe",
-// //         name: "Cat2"
-// //     },
-// //         {
-// //         id: "zxc",
-// //         name: "Cat3"
-// //     },
-// //         {
-// //         id: "rty",
-// //         name: "Cat4"
-// //     },
-// //         {
-// //         id: "fgh",
-// //         name: "Cat5"
-// //     },
-// //         {
-// //         id: "vbn",
-// //         name: "Cat6"
-// //     },
-    
-// // ];
 
-// // // Класичний for
-// // // for (let i = 0; i < numbers.length; i += 1) {
-// // //   console.log(`Індекс ${i}, значення ${numbers[i]}`);
-// // // }
 
-// // // Метод перебирання forEach
-// // cats.forEach(function (cat) {
-// //   console.log(`значення ${cat.id}, Ім'я ${cat.name}`);
-// // });
-
-// const myUsers = [
-//     { name: 'shark', likes: 'ocean' },
-//     { name: 'turtle', likes: 'pond' },
-//     { name: 'otter', likes: 'fish biscuits' }
-// ]
-
-// const usersByLikes = myUsers.map(item => {
-//      console.log(item.name);
-//     console.log(item.name.length);
-//     const container = {};
-//     console.log(container);
-
-//     container[item.name] = item.likes;
-//     container.age = item.name.length * 10;
-
-//     return container;
-// })
-
-// console.log(usersByLikes)
