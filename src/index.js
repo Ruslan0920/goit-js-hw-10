bcfc4f31aceaaa2194e1e207141670947001772b
Ruslan0920 <rus_boy2002@ukr.net>
2023-11-16 09:46:33 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ console.log(errorMessage);
 const descriptionCatInfo = document.querySelector('.cat-info');
 console.log(descriptionCatInfo);
  
-cardContainer.addEventListener("submit", selectCat);
+cardContainer.addEventListener("change", selectCat);
 
     cardContainer.classList.add('is-hidden');
     descriptionCatInfo.classList.add('is-hidden');
@@ -63,11 +63,11 @@ function selectCat(event) {
     
       const breedId = event.currentTarget.value;
     console.log(breedId);
-
+loaderWait.classList.remove('is-hidden');
     
     fetchCatByBreed(breedId)
         .then(data => {
-            // loaderWait.classList.add('is-hidden');
+            
             
       console.log(data);
       console.log(data[0]);
@@ -78,7 +78,8 @@ function selectCat(event) {
       console.log(data[0]);
       //   console.log(breeds[0].temperament);
       descriptionCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
-descriptionCatInfo.classList.remove('is-hidden');
+            descriptionCatInfo.classList.remove('is-hidden');
+            loaderWait.classList.add('is-hidden');
         });
    
 };
