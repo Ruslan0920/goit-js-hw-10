7e37509945a034197bb3755f866433401e2a3af2
Ruslan0920 <rus_boy2002@ukr.net>
2023-12-05 16:09:03 +0200

    ups

--- src/index.js
+++ src/index.js
@@ -105,8 +105,8 @@ loaderWait.classList.remove('is-hidden');
     fetchCatByBreed(breedId)
         .then(data => {
 console.log(data);
-      const { url, breeds } = data[0];
-      console.log(data[0]);
+      const { url, breeds : {description: catDescription}, {name: catName}, {temperament: catTemperament} } = data[0];
+      console.dir(data[0]);
       //   console.log(breeds[0].temperament);
     // descriptionCatInfo.classList.remove('is-hidden');
              if (breeds[0].id === breedId) {
